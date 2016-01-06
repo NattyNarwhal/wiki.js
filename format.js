d6684c0eaf40ca360148bb813e6c4dadcf9ecab5
Calvin <calvin@openmailbox.org>
2016-01-05 20:09:54 -0400

    basic redlinks

--- format.js
+++ format.js
@@ -1,12 +1,18 @@
-﻿var	fs         = require("fs"),
-    marked     = require("marked");
+﻿var fs      = require("fs"),
+    path    = require("path"),
+    sprintf = require("sprintf-js"),
+    marked  = require("marked");
 
+// Config options
+var wikidir = "./wiki";
 marked.setOptions({
+    // TODO: does this make sense? makeLinks should be using a semantic span tag/class attrib..
     sanitize: true,
+    gfm: true
 });
 
 var camelCase = /[A-Z][a-z]*[A-Z][a-z]*\b/g;
-var link = "[$&]($&)";
+var link = "[%s](%s)";
 
 // mapping example: [[/%TITLE%/g,"Title"],[/%META%/g,"Metadata"]]
 // access via iter of array, then first el = regex; second el = to replace with
@@ -17,8 +23,18 @@ exports.formatTemplate = function (template, mappings) {
     return template;
 }
 
+exports.makeLinks = function (match, offset, whole) {
+    var final = match;
+    // check for "redlinks"
+    if (!fs.existsSync(path.join(wikidir, match))) {
+        final = sprintf.sprintf("*%s*", final);
+    }
+    final = sprintf.sprintf(link, final, match);
+    return final;
+}
+
 exports.parsePage = function(content) {
-	return marked(content.replace(camelCase, link));
+	return marked(content.replace(camelCase, this.makeLinks));
 }
 
 exports.getMtime = function(fileName) {
