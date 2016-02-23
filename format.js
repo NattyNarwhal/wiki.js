f86ed233569b016e3a5dd0d8db3e3e0295a2b9ac
Calvin <calvin@openmailbox.org>
2016-02-23 10:42:30 -0400

    handle redlinks inside of the renderer

--- format.js
+++ format.js
@@ -5,14 +5,44 @@
 
 // Config options
 var wikidir = "./wiki";
-marked.setOptions({
-    // TODO: does this make sense? makeLinks should be using a semantic span tag/class attrib..
-    sanitize: true,
-    gfm: true
-});
 
 var camelCase = /\b((?:[A-Z][a-z]+){2,})(?![^()\[\]]*[)\]])\b/g;
 var link = "[%s](%s)";
+
+// override the link function to provide a class for non-existant pages
+var renderer = new marked.Renderer();
+renderer.link = function (href, title, text) {
+    if (marked.options.sanitize) {
+        try {
+            var prot = decodeURIComponent(unescape(href))
+        .replace(/[^\w:]/g, '')
+        .toLowerCase();
+        } catch (e) {
+            return '';
+        }
+        if (prot.indexOf('javascript:') === 0 || prot.indexOf('vbscript:') === 0) {
+            return '';
+        }
+    }
+    var out = '<a href="' + href + '"';
+    if (title) {
+        out += ' title="' + title + '"';
+    }
+    // this statement is the new clause from the marked built-in
+    // TODO: match only camelcase
+    if (href.match(camelCase) && !fs.existsSync(path.join(wikidir, href))) {
+        out += ' class="redlink"';
+    }
+    out += '>' + text + '</a>';
+    return out;
+};
+
+marked.setOptions({
+    // TODO: does this make sense? makeLinks should be using a semantic span tag/class attrib..
+    sanitize: true,
+    gfm: true,
+    renderer: renderer
+});
 
 // mapping example: [[/%TITLE%/g,"Title"],[/%META%/g,"Metadata"]]
 // access via iter of array, then first el = regex; second el = to replace with
@@ -25,10 +55,6 @@ exports.formatTemplate = function (template, mappings) {
 
 exports.makeLinks = function (match, offset, whole) {
     var final = match;
-    // check for "redlinks"
-    if (!fs.existsSync(path.join(wikidir, match))) {
-        final = sprintf.sprintf("*%s*", final);
-    }
     final = sprintf.sprintf(link, final, match);
     return final;
 }
